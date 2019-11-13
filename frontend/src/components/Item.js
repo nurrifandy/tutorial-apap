b773800715098b2f7a904de3d4fea6baeba7f451
lembaga asisten <lembaga.asisten@gmail.com>
2019-11-13 13:30:03 +0700

    menyelesaikan latihan 1

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -5,17 +5,34 @@ export default function Item(props){
     const {nama, deskripsi, harga, checked} = item;
 
     const handleChange=() => !!onChange && onChange({...item, checked: !checked});
-    return(
-        <button
-        type="button" className="list-group-item list-group-item-action flex-column align-items-start"
-        onClick={handleChange}
-        >
-            <div className="d-flex w-100 justify-content-between align-items-center">
-                <input type="checkbox" checked={checked} onChange={handleChange}/>
-                <h5 className="mb-1">{nama}</h5>
-                <small>Rp.{harga}</small>
-            </div>
-    <p className="mb-1">{deskripsi}</p>
-        </button>
-    )
+    if(!checked){
+        return(
+            <button
+            type="button" className="list-group-item list-group-item-action flex-column align-items-start"
+            onClick={handleChange}
+            >
+                <div className="d-flex w-100 justify-content-between align-items-center">
+                    <h5 className="mb-1">{nama}</h5>
+                    <small>Rp.{harga}</small>
+                </div>
+        <p className="mb-1">{deskripsi}</p>
+            </button>
+        )
+    }
+    else{
+        return(
+            <button
+            type="button" className="list-group-item list-group-item-action flex-column align-items-start"
+            onClick={handleChange}
+            >
+                <div className="d-flex w-100 justify-content-between align-items-center">
+                    <input type="checkbox" checked={checked} onChange={handleChange}/>
+                    <h5 className="mb-1">{nama}</h5>
+                    <small>Rp.{harga}</small>
+                </div>
+        <p className="mb-1">{deskripsi}</p>
+            </button>
+        )
+    }
+   
 }
